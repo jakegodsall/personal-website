30a7c0244111fbbfa2337be4c277da216888fee5
Jake Godsall <jake.edward.godsall@gmail.com>
2022-07-26 19:58:33 +0100

    removed uneccessary files from pycharm

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def main():
-    return "Hello, World!"
+def home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
